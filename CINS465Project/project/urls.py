36bc4912fdcf1212634781646512fdb490575e8d
densellp <densell321@gmail.com>
2022-05-06 13:00:43 -0700

    added some basic templates and pages

--- CINS465Project/project/urls.py
+++ CINS465Project/project/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.indexPage, name="indexPage"),
     path('testPage/', views.testPage, name="testPage"),
 ]
--- CINS465Project/project/views.py
+++ CINS465Project/project/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def testPage(request):
-    return HttpResponse('Hurray, you made it to the test page!')
+    #return HttpResponse('Hurray, you made it to the test page!')
+    return render(request, 'project/project.html')
+
+def indexPage(request):
+    return render(request, 'index.html')
