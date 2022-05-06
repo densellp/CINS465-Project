f3337e5517664a9e855e244063699b82c4cfda57
densellp <densell321@gmail.com>
2022-05-06 15:59:44 -0700

    Added links to other pages and django template language logic

--- CINS465Project/project/views.py
+++ CINS465Project/project/views.py
@@ -3,9 +3,32 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+testlist = [
+    {
+        'id': '1',
+        'title': "test file 1",
+        'description': 'this is test file 1',
+    },
+    {
+        'id': '2',
+        'title': "test file 2",
+        'description': 'this is test file 2',
+    },
+    {
+        'id': '3',
+        'title': "test file 3",
+        'description': 'this is test file 3',
+    },
+]
+
 def testPage(request):
     #return HttpResponse('Hurray, you made it to the test page!')
-    return render(request, 'project/project.html')
+    msg1 = 'TestPage'
+    number = 10
+    context = {'msg1':msg1, 'number':number, 'testlist':testlist}
+    return render(request, 'project/testpage.html', context)
 
 def indexPage(request):
-    return render(request, 'index.html')
+    msg2 = 'Index'
+    context = {'msg2':msg2}
+    return render(request, 'index.html', context)
