2fd78d0c37d3a11e8ad25b764cc0b023d8e18c12
densellp <densell321@gmail.com>
2022-05-06 18:37:28 -0700

    Added Basic Database and relationships

--- CINS465Project/project/admin.py
+++ CINS465Project/project/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import testTable, Profile, Comment, Post
+
+admin.site.register(testTable)
+admin.site.register(Profile)
+admin.site.register(Comment)
+admin.site.register(Post)
--- CINS465Project/project/models.py
+++ CINS465Project/project/models.py
@@ -1,3 +1,49 @@
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
+
+class testTable(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, 
+                        primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
+
+class Profile(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                        primary_key=True, editable=False)
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+class Post(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                        primary_key=True, editable=False)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, null=True)
+    body = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                        primary_key=True, editable=False)
+    post = models.ForeignKey(Post, null=True, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.profile.user.username
